05d6eac21f110f354b6130545f82320e6476f944
Ahmed Amin <AhmedAminHassanIsmail@gmail.com>
2022-11-28 23:04:55 +0200

    initial commit with initial project file

---
+++ SaveRecipe/SaveRecipe/SaveRecipeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SaveRecipeApp.swift
+//  SaveRecipe
+//
+//  Created by Ahmed Amin on 28/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct SaveRecipeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
